54d2707c70a2b12f34161c1ef73be4e24880ac2c
kingleonardo999 <1990408695@qq.com>
2025-07-02 13:20:17 +0800

    配置更改

--- src/config/baseURL.ts
+++ src/config/baseURL.ts
@@ -1,3 +1,3 @@
-export const baseURL_dev:string = 'http://localhost:5000'
+export const baseURL_dev:string = 'http://192.168.31.153:5000'
 export const baseURL_pro:string = 'http://localhost:5000'
 export const baseURL_test:string = 'http://localhost:5000'
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   host: '0.0.0.0',   // 允许外部设备访问
-  //   port: 5137,         // 如果需要，设置端口为你希望使用的端口
-  //   strictPort: true,   // 确保端口已被占用时不切换端口
-  //   allowedHosts: [
-  //     'king',  // 允许 "king" 主机
-  //   ],
-  // }
+  server: {
+    host: '0.0.0.0',   // 允许外部设备访问
+    port: 5137,         // 如果需要，设置端口为你希望使用的端口
+    strictPort: true,   // 确保端口已被占用时不切换端口
+    allowedHosts: [
+      'king',  // 允许 "king" 主机
+    ],
+  }
 })
